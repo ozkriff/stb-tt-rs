34a27b96e4f9b5b2843060982ab4821335a9d26c
ozkriff <ozkriff@gmail.com>
2014-04-10 22:59:53 +0400

    STBTT_{malloc,free}

--- stb_truetype.c
+++ stb_truetype.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define STBTT_malloc(x,u)  malloc(x)
+#define STBTT_free(x,u)    free(x)
 
 // force following include to generate implementation
 #define STB_TRUETYPE_IMPLEMENTATION
