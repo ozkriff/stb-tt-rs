f11081d24025be94f2a49b7679a81fa6abc6b8b8
Andrey Lesnikov <ozkriff@gmail.com>
2015-03-19 09:20:32 +0300

    build.rs: Updated for cargo 66849de 2015-03-10

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 extern crate gcc;
 
 fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
     gcc::compile_library(
         "libstb_truetype.a", &["stb_truetype.c"]);
+    println!("cargo:rustc-flags=-L native={}", out_dir);
 }
